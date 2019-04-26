a98aa9d0906ad18a2cf72ef728a668e81d9819cf
Amrit Pal Singh <amrit.sbagga@gmail.com>
2019-04-26 12:11:18 +0530

    Updated azureRemove.js
    Commented delete Resource group to avoid deleting used resource group since we have requirement to use same group & it may contain other azure services required for project

--- remove/azureRemove.js
+++ remove/azureRemove.js
@@ -20,7 +20,7 @@ class AzureRemove {
       'remove:remove': () => BbPromise.bind(this)
         .then(this.provider.initialize(this.serverless,this.options))
         .then(this.loginToAzure)
-        .then(this.deleteResourceGroup)
+       // .then(this.deleteResourceGroup)
         .then(() => this.serverless.cli.log('Service successfully removed'))
     };
   }
